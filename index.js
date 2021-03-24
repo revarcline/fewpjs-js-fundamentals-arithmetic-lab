e47dca48b8e0e47ecdcdf4a9c338b1e75b9d593c
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-24 16:11:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(2, 3, 19, 20);
